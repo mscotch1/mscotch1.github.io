ad524d5d3ab0de4346a9e46cebe45da4bfe5a063
Michael Scott <michael.yates.scott@gmail.com>
2020-11-26 05:37:05 -0600

    Use webpack to bundle everything together
    Now using bundled and customized bootstrap

---
+++ public/index.js
@@ -0,0 +1,9 @@
+// 3rd party imports
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// local imports
+// js
+
+// style
+import './style/main.scss';
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// webpack.config.js
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './public/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                indentWidth: 4,
+                includePaths: [
+                  "public/style",
+                ],
+              },
+            },
+          },
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 8080,
+    publicPath: "http://localhost:8080/dist/",
+  },
+}
