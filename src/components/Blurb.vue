9d08fd3980b47342018eb954d7785540c20dc1cb
Michael Scott <michael.yates.scott@gmail.com>
2021-10-01 15:41:48 -0500

    Replace popovers with standard titles

--- src/components/Blurb.vue
+++ src/components/Blurb.vue
@@ -1,9 +1,6 @@
 <template>
   <img
-    ref="image"
-    data-bs-toggle="popover"
-    :title="header"
-    :data-bs-content="credit"
+    :title="credit"
     :src="image"
     :alt="alt"
     class="p-3"
@@ -11,27 +8,12 @@
 </template>
 
 <script>
-  import Popover from 'bootstrap/js/dist/popover';
   export default {
     props: {
       header: String,
       alt: String,
       credit: String,
       image: String,
-    },
-    created() {
-      this.$nextTick(() => {
-        new Popover(
-          this.$refs.image,
-          {
-            html: true,
-            trigger: 'click',
-            placement: 'auto',
-            container: 'body',
-            boundary: 'viewport',
-          },
-        );
-      });
     },
   };
 </script>
